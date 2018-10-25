030b536e5d36cafdfc84f254c6f13085cd43ced1
richard <richard@mars.rac.local>
2018-10-25 13:04:05 -0400

    Separated files

--- src/main.cpp
+++ src/main.cpp
@@ -3,138 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
-#include <math.h>
-#include <cmath>
 
+#include "solution.hpp"
 #include "matplotlibcpp.h"
 
 namespace plt = matplotlibcpp;
 
 
-class Solution ;
 Solution * ode( double a, double b, double c, double t0, double y0, double yp0 ) ;
-
-class Solution {
-	public :
-		virtual const std::string toString() = 0 ;
-		virtual double y( double t ) = 0 ;
-} ;
-
-class Simple : public Solution {
-	private:
-		double r1 ;
-		double r2 ;
-		double c2  ;
-		double c1 ;
-		double toff ;
-
-	public :
-		const std::string toString() {
-			char buf[200] ;
-			sprintf( buf, "y = %.2f e^%.2ft  %+.2f e^%.2ft" , c1, r1, c2, r2 ) ; 
-			std::string rc = buf ;
-			return rc ;
-		} 
-		double y( double t ) {
-			double y = c1 * exp( r1 * (t+toff) ) + c2 * exp( r2 * (t+toff) ) ;
-		}
-		Simple( double a, double b, double c, double t0, double y0, double yp0 ) {
-			r1 = ( -b + sqrt( b*b - 4*a*c ) ) / ( 2 * a ) ;
-			r2 = ( -b - sqrt( b*b - 4*a*c ) ) / ( 2 * a ) ;
-
-			// y0 = c1 + c2
-			// y' = r1*c1 + r2*c2
-			// 
-			// so...
-			// 	c1 = y0 - c2
-			// 	yp0 = ( r1 * ( y0-c2 ) ) + r2*c2
-			//      = r1*y0 - r1*c2 + r2*c2
-			//      = r1*y0 + c2( r2 - r1 )
-			//  c2 = ( yp0 - r1*y0 ) / ( r2 - r1 )
-
-			c2 = ( yp0 - r1*y0 ) / ( r2 - r1 ) ;
-			c1 = y0 - c2 ;
-			toff = -t0 ;
-		}
-} ;
-
-
-class Single : public Solution {
-	private:
-		double r ;
-		double c2  ;
-		double c1 ;
-		double toff ;
-
-	public :
-		const std::string toString() {
-			char buf[200] ;
-			sprintf( buf, "y = %.2f e^%.2ft  %+.2f t e^%.2ft" , c1, r, c2, r ) ;
-			std::string rc = buf ;
-			return rc ;
-		} 
-		double y( double t ) {
-			double y = c1 * exp( r * (t-toff) ) + c2 * (t-toff) * exp( r * (t-toff) ) ;
-		}
-		Single( double a, double b, double c, double t0, double y0, double yp0 ) {
-			r =  -b / ( 2 * a ) ;
-
-			// y0 = c1
-			// y' = r*c1 + c2
-			// 
-			// so...
-			// c1 = y0
-			// c2 = yp0 - r*y0
-
-			c1 = y0 ;
-			c2 = yp0 - r * y0 ;
-			toff = -t0 ;
-		}
-} ;
-
-
-
-class Complex : public Solution {
-	private:
-		double re ;
-		double im ;
-		double c2  ;
-		double c1 ;
-		double toff ;
-
-	public :
-		const std::string toString() {
-			char buf[200] ;
-			sprintf( buf,"y = %.2f e^%.2ft cos(%.2f t ) %+.2f e^%.2ft sin(%.2f t )" , 
-				c1, re, im, c2, re, im ) ;
-			std::string rc = buf ;
-			return rc ;
-		} 
-		double y( double t ) {
-			double y = c1 * exp( re * (t-toff) ) + cos( im * (t-toff) ) +
-					   c2 * exp( re * (t-toff) ) + sin( im * (t-toff) ) ;
-		}
-		Complex( double a, double b, double c, double t0, double y0, double yp0 ) {
-			re = ( -b / (2*a) ) ;
-			im = sqrt( 4*a*c - b*b ) / ( 2 * a ) ;
-
-			//
-			// y = c1 * e^re*t cos( im*t) + c2 * e^re*t sin( im*t)
-			// y' = e ^re t ( sin( im * t) * ( c2*re - c1*im ) +  cos( im * t) * ( c1*re + c2*im ) )  
-			// 
-			// @t=0
-			// y0 = c1
-			// yp0 = y0 * re + c2 * im
-			// 
-			// c1 = y0
-			// c2 = ( yp0 - ( y0 * re ) ) / im
-			//
-			c1 = y0 ;
-			c2 = ( yp0 - ( y0 * re ) ) / im ;
-			toff = -t0 ;
-		}
-} ;
-
 
 
 //#define DEBUG 
